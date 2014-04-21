20d9f71b9d342e67c83920de67d794e5d7730233
Robert Foy <robert.foy@gmail.com>
2014-04-21 18:53:18 +1000

    Added rebuild button

--- hudson-console-link.user.js
+++ hudson-console-link.user.js
@@ -11,10 +11,13 @@ for (var i = 0; i < ds.length; i++) {
   var consoleImage = "<img title=\"Console output\" alt=\"Console output\" src=\"/static/dc668826/images/16x16/terminal.gif\" border=\"0\" />"
   var notepadImage = "<img title=\"Text output\" alt=\"Console output\" src=\"/static/dc668826/images/16x16/notepad.gif\" border=\"0\" />"
   var configImage = "<img title=\"Settings\" alt=\"Settings\" src=\"/static/dc668826/images/16x16/setting.png\" border=\"0\" />"
+  var rebuildImage = "<img title=\"Settings\" alt=\"Settings\" src=\"/static/dc668826/images/16x16/clock.png\" border=\"0\" />"
 
   var consoleLink = "<a href=\"" + relativeLink +"console\">" + consoleImage + "</a>";
   var notepadLink = "<a href=\"" + relativeLink +"consoleText\">" + notepadImage + "</a>";
   var parametersLink = "<a href=\"" + relativeLink +"parameters\">" + configImage + "</a>";
   var injectedVarsLink = "<a href=\"" + relativeLink +"injectedEnvVars\">" + configImage + "</a>";
-  element.children[2].innerHTML += consoleLink + notepadLink + parametersLink + injectedVarsLink;
+  var rebuildLink = "<a href=\"" + relativeLink +"injectedEnvVars\">" + rebuildImage + "</a>";
+
+  element.children[2].innerHTML += consoleLink + notepadLink + parametersLink + injectedVarsLink + rebuildLink;
 }
