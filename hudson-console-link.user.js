91d8787ac480616c6fa445706479806129c1f2d2
Robert Foy <robert.foy@ventyx.abb.com>
2015-09-28 17:03:00 +1000

    Remove menu selector

--- hudson-console-link.user.js
+++ hudson-console-link.user.js
@@ -14,7 +14,14 @@ function addLinks(v) {
     var r = $(v).find('a.tip').attr('href'); var cl = CONSOLE_LINK.replace('{0}', r); var nl = NOTEPAD_LINK.replace('{0}', r);
     var pl = PARAMETERS_LINK.replace('{0}', r); var il = INJECTEDVARS_LINK.replace('{0}', r); var rl = REBUILD_LINK.replace('{0}', r);
     $(v).find('td.build-row-cell').prepend('<div style="width: 80px;float: left">'+cl+nl+pl+il+rl);
+    $(v).find($('#side-panel').css('width','375px'));
+    $(v).find($('.pane.build-name').css('width','60px'));
+    $(v).find($('.pane.build-details').css('width','130px'));
+    $(v).find($('.pane.build-controls').css('width','50px'));
+    $(v).find($('a.tip').css('padding-right', '0px'));
+    $(v).find($('a.tip').css('padding-left', '0px'));
 }
+$('#menuSelector').remove();
 var CONSOLE_IMAGE = '<img title="Console output" alt="Console output" src="/static/dc668826/images/16x16/terminal.gif" border="0" />'
 var NOTEPAD_IMAGE = '<img title="Text output" alt="Console output" src="/static/dc668826/images/16x16/notepad.gif" border="0" />'
 var CONFIG_IMAGE = '<img title="Settings" alt="Settings" src="/static/dc668826/images/16x16/setting.png" border="0" />'
@@ -24,11 +31,5 @@ var NOTEPAD_LINK = '<a href="{0}consoleText">' + NOTEPAD_IMAGE + '</a>';
 var PARAMETERS_LINK = '<a href="{0}parameters">' + CONFIG_IMAGE + '</a>';
 var INJECTEDVARS_LINK = '<a href="{0}injectedEnvVars">' + CONFIG_IMAGE + '</a>';
 var REBUILD_LINK = '<a href="{0}rebuild">' + REBUILD_IMAGE + '</a>';
-$('#side-panel').css('width','375px');
-$('.pane.build-name').css('width','60px');
-$('.pane.build-details').css('width','130px');
-$('.pane.build-controls').css('width','50px');
-$('a.tip').css('padding-right', '0px');
-$('a.tip').css('padding-left', '0px');
 $('.build-row.overflow-checked').each(addLinks);
 waitForKeyElements('.transitive', addLinks);
