6bd7b03f957826aa63412c0c4fc469e661c921d8
Robert Foy <robert.foy@gmail.com>
2015-09-28 14:23:25 +1000

    Update hudson-console-link.user.js

--- hudson-console-link.user.js
+++ hudson-console-link.user.js
@@ -1,23 +1,22 @@
 // ==UserScript==
-// @name           Hudson Console Icon
-// @include        http://aubne-s-dvl01b.dev.mincom.com:8080/*
-// @include        http://aubne-s-dvl01b.ventyx.abb.com:8080/*
+// @name          Hudson Console Icon
+// @include       http://awsjenkins.dev.mincom.com:8080/*
+// @include       http://awsjenkins.techops.ventyx.abb.com:8080/*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // ==/UserScript==
 
-var ds = document.getElementsByClassName("build-row no-wrap");
-for (var i = 0; i < ds.length; i++) {
-  var element = ds[i];
-  var relativeLink = element.getElementsByClassName('tip')[0].attributes[1].textContent
-  var consoleImage = "<img title=\"Console output\" alt=\"Console output\" src=\"/static/dc668826/images/16x16/terminal.gif\" border=\"0\" />"
-  var notepadImage = "<img title=\"Text output\" alt=\"Console output\" src=\"/static/dc668826/images/16x16/notepad.gif\" border=\"0\" />"
-  var configImage = "<img title=\"Settings\" alt=\"Settings\" src=\"/static/dc668826/images/16x16/setting.png\" border=\"0\" />"
-  var rebuildImage = "<img title=\"Settings\" alt=\"Settings\" src=\"/static/dc668826/images/16x16/clock.png\" border=\"0\" />"
-
-  var consoleLink = "<a href=\"" + relativeLink +"console\">" + consoleImage + "</a>";
-  var notepadLink = "<a href=\"" + relativeLink +"consoleText\">" + notepadImage + "</a>";
-  var parametersLink = "<a href=\"" + relativeLink +"parameters\">" + configImage + "</a>";
-  var injectedVarsLink = "<a href=\"" + relativeLink +"injectedEnvVars\">" + configImage + "</a>";
-  var rebuildLink = "<a href=\"" + relativeLink +"rebuild\">" + rebuildImage + "</a>";
-
-  element.children[2].innerHTML += rebuildLink + consoleLink + notepadLink + parametersLink + injectedVarsLink;
-}
+var CONSOLE_IMAGE = '<img title="Console output" alt="Console output" src="/static/dc668826/images/16x16/terminal.gif" border="0" />'
+var NOTEPAD_IMAGE = '<img title="Text output" alt="Console output" src="/static/dc668826/images/16x16/notepad.gif" border="0" />'
+var CONFIG_IMAGE = '<img title="Settings" alt="Settings" src="/static/dc668826/images/16x16/setting.png" border="0" />'
+var REBUILD_IMAGE = '<img title="Settings" alt="Settings" src="/static/dc668826/images/16x16/clock.png" border="0" />'
+var CONSOLE_LINK = '<a href="{0}console">' + CONSOLE_IMAGE + '</a>';
+var NOTEPAD_LINK = '<a href="{0}consoleText">' + NOTEPAD_IMAGE + '</a>';
+var PARAMETERS_LINK = '<a href="{0}parameters">' + CONFIG_IMAGE + '</a>';
+var INJECTEDVARS_LINK = '<a href="{0}injectedEnvVars">' + CONFIG_IMAGE + '</a>';
+var REBUILD_LINK = '<a href="{0}rebuild">' + REBUILD_IMAGE + '</a>';
+$('#side-panel').css('width','450px');
+$('.build-row.single-line').each(function(i,v) {
+    var r = $(this).find('a.tip').attr('href'); var cl = CONSOLE_LINK.replace('{0}', r); var nl = NOTEPAD_LINK.replace('{0}', r);
+    var pl = PARAMETERS_LINK.replace('{0}', r); var il = INJECTEDVARS_LINK.replace('{0}', r); var rl = REBUILD_LINK.replace('{0}', r);
+    $(this).prepend(cl+nl+pl+il+rl);
+});
